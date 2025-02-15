9096fc65f22772162eb9184cb72dfbf564d15489
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-02-16 02:12:21 +0600

    skeleton

--- FrontEnd/src/Component/Shirt.jsx
+++ FrontEnd/src/Component/Shirt.jsx
@@ -2,54 +2,139 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { ToastContainer } from "react-toastify";
 import { handleError } from "../Toast";
-import { Link } from 'react-router';
-
+import { Link } from "react-router";
 
 const Shirt = () => {
   const [category, setCategory] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const allCetagory = async () => {
+    setLoading(true);
+
     try {
       const response = await axios.get(
         "http://localhost:3000/api/v1/category/allCetagory"
       );
       setCategory(response.data.allCetagory);
+      setLoading(false);
     } catch (error) {
       handleError(error.message);
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     allCetagory();
   }, []);
+  const LoadingState = () => {
+    return (
+      <>
+        <div className="max-w-sm w-full p-4 bg-white shadow-md rounded-lg border">
+          {/* Skeleton Image */}
+          <div className="animate-pulse mb-4">
+            <div className="w-32 h-32 bg-gray-300 rounded-lg mx-auto"></div>
+          </div>
 
+          {/* Skeleton Text */}
+          <div className="animate-pulse space-y-4">
+            <div className="h-4 bg-gray-300 rounded w-1/2 mx-auto"></div>
+            <div className="h-4 bg-gray-300 rounded w-2/3 mx-auto"></div>
+          </div>
+        </div>
+        <div className="max-w-sm w-full p-4 bg-white shadow-md rounded-lg border">
+          {/* Skeleton Image */}
+          <div className="animate-pulse mb-4">
+            <div className="w-32 h-32 bg-gray-300 rounded-lg mx-auto"></div>
+          </div>
+
+          {/* Skeleton Text */}
+          <div className="animate-pulse space-y-4">
+            <div className="h-4 bg-gray-300 rounded w-1/2 mx-auto"></div>
+            <div className="h-4 bg-gray-300 rounded w-2/3 mx-auto"></div>
+          </div>
+        </div>
+        <div className="max-w-sm w-full p-4 bg-white shadow-md rounded-lg border">
+          {/* Skeleton Image */}
+          <div className="animate-pulse mb-4">
+            <div className="w-32 h-32 bg-gray-300 rounded-lg mx-auto"></div>
+          </div>
+
+          {/* Skeleton Text */}
+          <div className="animate-pulse space-y-4">
+            <div className="h-4 bg-gray-300 rounded w-1/2 mx-auto"></div>
+            <div className="h-4 bg-gray-300 rounded w-2/3 mx-auto"></div>
+          </div>
+        </div>
+        <div className="max-w-sm w-full p-4 bg-white shadow-md rounded-lg border">
+          {/* Skeleton Image */}
+          <div className="animate-pulse mb-4">
+            <div className="w-32 h-32 bg-gray-300 rounded-lg mx-auto"></div>
+          </div>
+
+          {/* Skeleton Text */}
+          <div className="animate-pulse space-y-4">
+            <div className="h-4 bg-gray-300 rounded w-1/2 mx-auto"></div>
+            <div className="h-4 bg-gray-300 rounded w-2/3 mx-auto"></div>
+          </div>
+        </div>
+        <div className="max-w-sm w-full p-4 bg-white shadow-md rounded-lg border">
+          {/* Skeleton Image */}
+          <div className="animate-pulse mb-4">
+            <div className="w-32 h-32 bg-gray-300 rounded-lg mx-auto"></div>
+          </div>
+
+          {/* Skeleton Text */}
+          <div className="animate-pulse space-y-4">
+            <div className="h-4 bg-gray-300 rounded w-1/2 mx-auto"></div>
+            <div className="h-4 bg-gray-300 rounded w-2/3 mx-auto"></div>
+          </div>
+        </div>
+        <div className="max-w-sm w-full p-4 bg-white shadow-md rounded-lg border">
+          {/* Skeleton Image */}
+          <div className="animate-pulse mb-4">
+            <div className="w-32 h-32 bg-gray-300 rounded-lg mx-auto"></div>
+          </div>
+
+          {/* Skeleton Text */}
+          <div className="animate-pulse space-y-4">
+            <div className="h-4 bg-gray-300 rounded w-1/2 mx-auto"></div>
+            <div className="h-4 bg-gray-300 rounded w-2/3 mx-auto"></div>
+          </div>
+        </div>
+      </>
+    );
+  };
   return (
-    <Link >
+    <Link>
       <div className="font-sans bg-gray-50 px-4 py-8">
         <div className="mx-auto lg:max-w-6xl md:max-w-4xl">
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-            {category.map((item) => (
-              <div
-                key={item.id} // Make sure to add a unique key
-                className="bg-white p-3 cursor-pointer shadow-sm rounded-md hover:scale-[1.03] transition-all"
-              >
-                <div className="w-full overflow-hidden mx-auto">
-                  <img
-                    src={item.image}
-                    alt="product1"
-                    className="aspect-[108/82] w-full object-contain"
-                  />
+            {loading ? (
+              <LoadingState />
+            ) : (
+              category.map((item) => (
+                <div
+                  key={item.id} // Make sure to add a unique key
+                  className="bg-white p-3 cursor-pointer shadow-sm rounded-md hover:scale-[1.03] transition-all"
+                >
+                  <div className="w-full overflow-hidden mx-auto">
+                    <img
+                      src={item.image}
+                      alt="product1"
+                      className="aspect-[108/82] w-full object-contain"
+                    />
+                  </div>
+                  <div className="text-center mt-4">
+                    <h3 className="text-sm font-bold text-gray-800">
+                      {item.name}
+                    </h3>
+                    <h4 className="text-sm text-blue-600 font-bold mt-2">
+                      {item.description}
+                    </h4>
+                  </div>
                 </div>
-                <div className="text-center mt-4">
-                  <h3 className="text-sm font-bold text-gray-800">
-                    {item.name}
-                  </h3>
-                  <h4 className="text-sm text-blue-600 font-bold mt-2">
-                    {item.description}
-                  </h4>
-                </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
         </div>
       </div>
--- FrontEnd/src/Component/ShopSidebar.jsx
+++ FrontEnd/src/Component/ShopSidebar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import { Slider } from "@material-tailwind/react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { handleError } from "../Toast";
+import  axios  from 'axios';
 
 const ShopSidebar = () => {
+  const [category, setCategory] = useState([]);
+
+  const allCetagory = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/v1/category/allCetagory"
+      );
+      setCategory(response.data.allCetagory);
+    } catch (error) {
+      handleError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    allCetagory();
+  }, []);
+
   return (
     <>
       <div className="shadow-sm rounded-md shadow-gray-400 p-3">
@@ -9,31 +30,19 @@ const ShopSidebar = () => {
           Category
         </h2>
         <ul>
-          <li className="font-roboto hover:text-primary cursor-pointer select-none text-lg mt-2 text-black font-medium">
-            Ti-Shirt
-          </li>
-          <li className="font-roboto hover:text-primary cursor-pointer select-none text-lg mt-2 text-black font-medium">
-            Shirt
-          </li>
-          <li className="font-roboto hover:text-primary cursor-pointer select-none text-lg mt-2 text-black font-medium">
-            Shoes
-          </li>
-          <li className="font-roboto hover:text-primary cursor-pointer select-none text-lg mt-2 text-black font-medium">
-            Sari
-          </li>
-          <li className="font-roboto hover:text-primary cursor-pointer select-none text-lg mt-2 text-black font-medium">
-            Watch
-          </li>
-          <li className="font-roboto hover:text-primary cursor-pointer select-none text-lg mt-2 text-black font-medium">
-            Glass
-          </li>
-          <li className="font-roboto hover:text-primary cursor-pointer select-none text-lg mt-2 text-black font-medium">
-            Phone
-          </li>
-          <li className="font-roboto hover:text-primary cursor-pointer select-none text-lg mt-2 text-black font-medium">
-            Laptop
-          </li>
+          {
+            category.map((item) => (
+              <li
+                key={item._id}
+                className="font-roboto hover:text-primary cursor-pointer select-none text-lg mt-2 text-black font-medium"
+              >
+                {item.name}
+              </li>
+        
+    ))
+          }
         </ul>
+        
       </div>
       {/* price filter// */}
       <div className="shadow-sm rounded-md shadow-gray-400 p-3 mt-4">
