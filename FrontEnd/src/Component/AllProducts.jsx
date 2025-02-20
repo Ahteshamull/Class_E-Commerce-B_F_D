00d81e33cf1a9018f9ccc16232fcd339985d7c68
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-02-20 20:49:47 +0600

    all products

--- FrontEnd/src/Component/AllProducts.jsx
+++ FrontEnd/src/Component/AllProducts.jsx
@@ -69,17 +69,7 @@ const AllProducts = () => {
                     {item.stock}
                   </p>
                 </div>
-                <Link to={"/card"}>
-                  <div className="flex space-x-4  mb-6">
-                    <button
-                      onClick={() => handleClick(allProductsState)}
-                      className="bg-indigo-600 flex gap-2 items-center text-white px-6 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                      Add to Cart
-                    </button>
-                    
-                  </div>
-                </Link>
+               
               </div>
             </div>
           </div>
