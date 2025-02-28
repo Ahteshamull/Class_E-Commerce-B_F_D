62900875d9f3760c6e28984467818c9ceedd3bb9
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-02-28 23:41:43 +0600

    Dashboard = new

--- Dashboard/src/pages/AddProduct.jsx
+++ Dashboard/src/pages/AddProduct.jsx
@@ -164,7 +164,7 @@ const AddProduct = () => {
         </div>
 
         <div>
-          <label className="block text-lg font-medium text-gray-700">
+          <label className="block text-lg font-medium text-gray-600">
             Category
           </label>
           <select
