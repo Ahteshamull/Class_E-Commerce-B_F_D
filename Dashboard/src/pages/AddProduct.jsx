1ef07b77e818dc4eb4a376138b443847b9422248
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-02-28 23:56:40 +0600

    color change

--- Dashboard/src/pages/AddProduct.jsx
+++ Dashboard/src/pages/AddProduct.jsx
@@ -202,7 +202,7 @@ const AddProduct = () => {
         </div>
 
         <div>
-          <label className="block text-lg font-medium text-gray-700">
+          <label className="block text-lg font-medium text-gray-600">
             Stock
           </label>
           <input
