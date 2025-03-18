0b12e6640b03ca4ebd73b063b7e0ff41104dd5e3
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-03-19 00:45:07 +0600

    Feature Product Card

--- FrontEnd/src/Component/Products.jsx
+++ FrontEnd/src/Component/Products.jsx
@@ -1,27 +1,26 @@
 import { Container } from "postcss";
 import React from "react";
+import { useNavigate } from "react-router";
 
 const Products = (
   {product}
 ) => {
+    const navigate = useNavigate();
+   const handleProductId = (item) => {
+     navigate(`/shop/view/${item}`);
+   };
   return (
     <div className="grid md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-5 grid-cols-3  mt-5">
       <div className="w-80 mt-5 bg-white shadow rounded">
-        
         <div className="h-48 w-[250px] bg-gray-200 flex flex-col justify-between p-4 bg-cover bg-center">
-         
-         
           <div>
-           
             <span className="uppercase text-xs bg-green-50 p-0.5 border-green-500 border rounded text-green-700 font-medium select-none">
-          
               available
             </span>
             <img src={product.image[0]} alt="" />
           </div>
         </div>
         <div className="p-4 flex flex-col items-center">
-       
           <p className="text-black font-light text-sm text-center">
             {product.name}
           </p>
@@ -31,8 +30,10 @@ const Products = (
           <p className="text-center text-gray-800 mt-1">
             ${product.discountPrice}
           </p>
-          <button className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50 mt-4 w-full flex items-center justify-center">
-           
+          <button
+            onClick={() => handleProductId(product._id)}
+            className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50 mt-4 w-full flex items-center justify-center"
+          >
             Add to order
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +42,6 @@ const Products = (
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
